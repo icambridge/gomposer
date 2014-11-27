8130a4ba37ef1711c49370599f4c9d804b8eaca7
Iain Cambridge <github@icambridge.me>
2014-11-27 19:33:47 +0000

    Add package structs

---
+++ package.go
@@ -0,0 +1,59 @@
+package gomposer
+
+type Package struct {
+	Name        string       `json:"name"`
+	Description string       `json:"description"`
+	Time        string       `json:"time"`
+	Maintainers []Maintainer `json:"maintainers"`
+	Versions    []Version    `json:"versions"`
+	Type        string       `json:"type"`
+	Repository  string       `json:"repository"`
+	Downloads   Downloads    `json:"downloads"`
+	Favers      int          `json:"favers"`
+}
+
+type Maintainer struct {
+	Name string `json:"name"`
+}
+
+type Author struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Role  string `json:"role"`
+}
+
+type Source struct {
+	Type      string `json:"type"`
+	Url       string `json:"url"`
+	Reference string `json:"reference"`
+	Shasum    string `json:"shasum"`
+}
+
+type Downloads struct {
+	Total   int `json:"total"`
+	Monthly int `json:"monthly"`
+	Daily   int `json:daily`
+}
+
+type Autoload struct {
+}
+
+// TODO rename since this going to be the name composer.json
+type Version struct {
+	Name              string            `json:"name"`
+	Description       string            `json:"description"`
+	Keywords          []string          `json:"keywords"`
+	Homepage          string            `json:"homepage"`
+	Version           string            `json:"version"`
+	VersionNormalized string            `json:version_normalized`
+	License           []string          `json:"license"`
+	Authors           Author            `json:"authors"`
+	Source            Source            `json:"source"`
+	Dist              Source            `json:"dist"`
+	Type              string            `json:"string"`
+	Time              string            `json:"time"`
+	Autoload          Autoload          `json:"autoload"`
+	Require           map[string]string `json:"require"`
+	RequireDev        map[string]string `json:"require-dev"`
+	Suggest           map[string]string `json:"suggest"`
+}
--- readme.md
+++ readme.md
@@ -2,3 +2,11 @@ Gomposer
 ========
 
 An attempt at building composer client in golang. WIP
+
+# things to do
+
+Get package data
+Resolve dependencies
+Read composer.json
+Write composer.lock
+Download package
