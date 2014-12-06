c99e4b7d280fa6c4dadb026ba576bf2b696ed9b0
Iain Cambridge <github@icambridge.me>
2014-12-06 14:56:34 +0000

    Optmize and improved correctness

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"sort"
 	"github.com/codegangsta/cli"
+	"gomposer"
 	"os"
 )
 
 func main() {
+
 	app := cli.NewApp()
 	app.Name = "gomposer"
 	app.Usage = "Composer is go bro"
@@ -17,14 +17,14 @@ func main() {
 			Usage: "Updates",
 			Action: func(c *cli.Context) {
 
-				r := PackageReader{}
+				r := gomposer.PackageReader{}
 				actual, _ := r.Read("composer.json")
 
-				m := make(map[string]*PackageInfo)
-				hc, _ := NewHttpClient("https://packagist.org/packages/")
-				pr := PackageRepository{Client: hc}
+				m := make(map[string]*gomposer.PackageInfo)
+				hc, _ := gomposer.NewHttpClient("https://packagist.org/packages/")
+				pr := gomposer.PackageRepository{Client: hc}
 
-				p := Process{PackageRepo: &pr, Packages: m}
+				p := gomposer.Process{PackageRepo: &pr, Packages: m}
 				l := p.Process(actual)
 
 				names := []string{}
@@ -34,11 +34,7 @@ func main() {
 					version[v.Name] = v.Version
 				}
 
-				sort.Strings(names)
-				for _, name := range names {
-					fmt.Println(name + "->" + version[name])
-				}
-
+				gomposer.WriteLock(l)
 			},
 		},
 	}
