0345d48659e33ebef0c2d1d57e0d2788c5db4dd2
Iain Cambridge <github@icambridge.me>
2014-12-16 18:02:28 +0000

    PoC code unzip

--- unzip.go
+++ unzip.go
@@ -1,9 +1,71 @@
 package main
 
 import (
+	"archive/zip"
 	"fmt"
+	"log"
+	"os"
+	"strings"
+	"path/filepath"
+	"io"
 )
 
 func main() {
-	fmt.Println("Hello")
+	r, err := zip.OpenReader("output.zip")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Close()
+
+	// Iterate through the files in the archive,
+	// printing some of their contents.
+	pkgName := "alexandresalome/PHP-Selenium"
+	dirs := strings.Split(pkgName, "/")
+	dirName := "vendors"
+	for _, k := range dirs {
+		dirName = dirName + "/" + k
+		os.Mkdir(dirName, 0777)
+	}
+	//os.Mkdir("vendors/"+pkgName, 0777)
+	for _, f := range r.File {
+
+		fileName := filepath.Base(f.Name)
+		if fileName == "." {
+			continue
+		}
+		currentDir := dirName
+
+		rc, err := f.Open()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		parts := strings.Split(filepath.Dir(f.Name), "/")
+		parts = parts[1:]
+
+		for _, k := range parts {
+			currentDir = currentDir + "/" + k
+			os.Mkdir(currentDir, 0777)
+		}
+		partCount := len(parts)
+
+		if partCount > 0 && parts[partCount-1] == fileName {
+			continue
+		}
+
+		of, err := os.Create(dirName + "/" + fileName)
+		if err != nil {
+			fmt.Println(dirName + "/" + fileName)
+			log.Fatal(err)
+		}
+		_, err = io.Copy(of, rc)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		rc.Close()
+		of.Close()
+	}
 }
+
+
