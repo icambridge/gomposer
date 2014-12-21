db3d43323b8994a84c6c5f50f190f9c93222f665
Iain Cambridge <github@icambridge.me>
2014-12-21 19:17:40 +0000

    Add quick Remove

--- gomposer/file_system_test.go
+++ gomposer/file_system_test.go
@@ -5,10 +5,24 @@ import (
 	"os"
 )
 
-func Test_Deletes_Folder(t *testing.T) {
+func Test_Remove_Deletes_Folder(t *testing.T) {
 	vendorDir := os.TempDir()  + "/vendors"
 	dirName := vendorDir+"/symfony/symfony"
 	os.MkdirAll(dirName, 0744)
+	v := Version{
+		Name: "symfony/symfony",
+	}
+	Remove(vendorDir, v)
+
+	if _, err := os.Stat(dirName); !os.IsNotExist(err) {
+		t.Errorf("Failed to delete %v", dirName)
+	}
+}
+
+func Test_Remove_Deletes_Parent_Folder(t *testing.T) {
+	vendorDir := os.TempDir()  + "/vendors"
+	dirName := vendorDir+"/symfony"
+	os.MkdirAll(dirName + "/symfony", 0744)
 	v := Version{
 		Name: "symfony/symfony",
 	}
